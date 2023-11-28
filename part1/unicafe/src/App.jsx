6c81b300e5038711be1dbfec2f4899baa0d733e5
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-28 03:34:35 +0200

    ex1.6 part 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const goodClicking = () => {
+    setGood(good + 1);
+  };
+
+  const neutralClicking = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const badClicking = () => {
+    setBad(bad + 1);
+  };
+
   return (
     <div>
       <h1>give feedback</h1>
-      <button>good</button>
-      <button>neutral</button>
-      <button>bad</button>
+      <button onClick={goodClicking}>good</button>
+      <button onClick={neutralClicking}>neutral</button>
+      <button onClick={badClicking}>bad</button>
       <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 };
