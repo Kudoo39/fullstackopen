7bca5e3d0fd2937d5b7f9d1bcdcf8338b846e038
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-28 23:14:34 +0200

    ex1.7 step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,21 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const goodClicking = () => {
     setGood(good + 1);
+    setTotal(total + 1);
   };
 
   const neutralClicking = () => {
     setNeutral(neutral + 1);
+    setTotal(total + 1);
   };
 
   const badClicking = () => {
     setBad(bad + 1);
+    setTotal(total + 1);
   };
 
   return (
@@ -28,6 +32,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {(100 * good) / total} %</p>
     </div>
   );
 };
