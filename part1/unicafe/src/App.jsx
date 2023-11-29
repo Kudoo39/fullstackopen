75119a665bbb898dea36cfc7c09aef52e0e7db73
unknown <thpthv.nguyenphuduykhang@gmail.com>
2023-11-29 02:52:10 +0200

    ex1.10 step 5 split 2 components

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 
+const StatisticLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
 const Statistics = ({ good, neutral, bad, total }) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>good {good}</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={(good - bad) / total} />
+      <StatisticLine text="positive" value={`${(100 * good) / total}%`} />
+
+      {/* <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {total}</p>
       <p>average {(good - bad) / total}</p>
-      <p>positive {(100 * good) / total} %</p>
+      <p>positive {(100 * good) / total} %</p> */}
     </div>
   );
+};
+
+const Button = ({ click, text }) => {
+  return <button onClick={click}>{text}</button>;
 };
 
 const App = () => {
@@ -37,9 +56,12 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={goodClicking}>good</button>
+      <Button click={goodClicking} text="good" />
+      <Button click={neutralClicking} text="neutral" />
+      <Button click={badClicking} text="bad" />
+      {/* <button onClick={goodClicking}>good</button>
       <button onClick={neutralClicking}>neutral</button>
-      <button onClick={badClicking}>bad</button>
+      <button onClick={badClicking}>bad</button> */}
       {total === 0 && <p>No feedback given</p>}
       {total !== 0 && (
         <Statistics good={good} neutral={neutral} bad={bad} total={total} />
